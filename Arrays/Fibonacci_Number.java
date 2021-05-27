b3cc948687d6878671b29ffcef894f83859a84d7
Javeed Suhail <javeed3157@gmail.com>
2021-05-27 19:05:03 +0530

    509

---
+++ Arrays/Fibonacci_Number.java
@@ -0,0 +1,23 @@
+public class Fibonacci_Number{
+    public static int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        // return fib(n-1)+fib(n-2);
+        int a=0,b=1,c=0;
+        while(n>1){
+            c=a+b;
+            a=b;
+            b=c;
+            n-=1;
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        int k=4;
+        System.out.println(fib(k));
+    }
+}
