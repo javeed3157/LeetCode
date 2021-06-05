c0dbd5a34d966ac2c29e796802c13de9299d613c
Javeed Suhail <javeed3157@gmail.com>
2021-06-05 17:56:52 +0530

    48

---
+++ Arrays/Rotate_Image.java
@@ -0,0 +1,30 @@
+//Time:O(n^2)
+//Space:O(1)
+public class Rotate_Image {
+    public static void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j]=temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        rotate(arr);
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
