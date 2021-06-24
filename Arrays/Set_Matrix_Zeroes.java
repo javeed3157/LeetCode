ac2d33403f638d6e3022359dc83709ac7f3d2064
Javeed Suhail <javeed3157@gmail.com>
2021-06-24 22:53:37 +0530

    73

--- Arrays/Set_Matrix_Zeroes.java
+++ Arrays/Set_Matrix_Zeroes.java
@@ -1,3 +1,5 @@
+//Time:O(mn)
+//Space:O(1)
 public class Set_Matrix_Zeroes{
     // Brute Force Approach (Time:O(mn)*O(m+n) Space:O(1))
     // public static void setZeroes(int[][] matrix) {
@@ -44,32 +46,64 @@ public class Set_Matrix_Zeroes{
     // }
     //Time:O(mn)
     //Space:O(m+n)
-    public static void setZeroes(int[][] matrix) {
-    int row[]=new int[matrix.length];
-    int col[]=new int[matrix[0].length];
-    for(int i=0;i<matrix.length;i++){
-        for(int j=0;j<matrix[0].length;j++){
-            if(matrix[i][j]==0){
-                row[i]=1;
-                col[j]=1;
-            }
-        }
-    }
-    for(int i=0;i<matrix.length;i++){
-        for(int j=0;j<matrix[0].length;j++){
-            if(row[i]==1 || col[j]==1){
-                matrix[i][j]=0;
-            }
-        }
-    }
-    for(int i=0;i<matrix.length;i++){
-        for(int j=0;j<matrix[0].length;j++){
-            System.out.print(matrix[i][j]);
-        }
-        System.out.println();
-    }
-    }
+    // public static void setZeroes(int[][] matrix) {
+    // int row[]=new int[matrix.length];
+    // int col[]=new int[matrix[0].length];
+    // for(int i=0;i<matrix.length;i++){
+    //     for(int j=0;j<matrix[0].length;j++){
+    //         if(matrix[i][j]==0){
+    //             row[i]=1;
+    //             col[j]=1;
+    //         }
+    //     }
+    // }
+    // for(int i=0;i<matrix.length;i++){
+    //     for(int j=0;j<matrix[0].length;j++){
+    //         if(row[i]==1 || col[j]==1){
+    //             matrix[i][j]=0;
+    //         }
+    //     }
+    // }
+    // for(int i=0;i<matrix.length;i++){
+    //     for(int j=0;j<matrix[0].length;j++){
+    //         System.out.print(matrix[i][j]);
+    //     }
+    //     System.out.println();
+    // }
+    // }
 
+    public static void setZeroes(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int colze=1;
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                colze=0;
+            }
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=row-1;i>=0;i--){
+            for(int j=col-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if(colze==0){
+                matrix[i][0]=0;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // int arr[][]={{1,1,1},{1,0,1},{1,1,1}};
         int arr[][]={{0,1,2,0},{3,4,5,2},{1,3,1,5}};
