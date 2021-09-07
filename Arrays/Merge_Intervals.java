490ad0a744cab5c0c883c29f8c2c622ed3663946
Javeed Suhail <javeed3157@gmail.com>
2021-09-07 10:46:29 +0530

    56

--- Arrays/Merge_Intervals.java
+++ Arrays/Merge_Intervals.java
@@ -11,7 +11,7 @@ public class Merge_Intervals {
         }
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int start=intervals[0][0];
-        int end=intervals[0][0];
+        int end=intervals[0][1];
         for(int[] i:intervals){
             if(i[0]<=end){
                 end=Math.max(i[1],end);
