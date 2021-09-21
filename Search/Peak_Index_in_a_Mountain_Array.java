f254ee41fb1feab908360b7b8fb6a08743165485
Javeed Suhail <javeed3157@gmail.com>
2021-09-21 08:00:17 +0530

    852

--- Search/Peak_Index_in_a_Mountain_Array.java
+++ Search/Peak_Index_in_a_Mountain_Array.java
@@ -4,13 +4,13 @@ public class Peak_Index_in_a_Mountain_Array {
     public static int peakIndexInMountainArray(int[] arr) {
         int s=0;
         int e=arr.length-1;
-        while(s<e){
+        while(s<=e){
             int mid=s+(e-s)/2;
             if(arr[mid]<arr[mid+1]){
                 s=mid+1;
             }
             else{
-                e=mid;
+                e=mid-1;
             }
         }
         return s;
