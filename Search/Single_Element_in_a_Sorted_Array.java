282da5c3aec92b4afdbcce9ece0265be3317d6d4
Javeed Suhail <javeed3157@gmail.com>
2021-09-23 09:27:17 +0530

    540

---
+++ Search/Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,32 @@
+//Time:O(log(n))
+//Space:O(1)
+public class Single_Element_in_a_Sorted_Array {
+    public static int singleNonDuplicate(int[] nums) {
+        int l=0;
+        int h=nums.length-1;
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    l=mid+2;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+            else{
+                if(nums[mid]==nums[mid-1]){
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+        }
+        return nums[l];
+    }
+    public static void main(String[] args) {
+        int a[]={1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(a));
+    }
+}
