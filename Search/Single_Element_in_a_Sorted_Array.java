41aa70235fe8909880ba3d2f27327164d4676ba3
Javeed Suhail <javeed3157@gmail.com>
2021-09-24 19:44:21 +0530

    540

--- Search/Single_Element_in_a_Sorted_Array.java
+++ Search/Single_Element_in_a_Sorted_Array.java
@@ -27,6 +27,7 @@ public class Single_Element_in_a_Sorted_Array {
     }
     public static void main(String[] args) {
         int a[]={1,1,2,3,3,4,4,8,8};
+        // int a[] = {3,3,7,7,10,11,11};
         System.out.println(singleNonDuplicate(a));
     }
 }
