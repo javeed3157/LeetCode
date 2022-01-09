ff1c9725b672289d44bff1002a2cc1f2375c1da2
Javeed Suhail <javeed3157@gmail.com>
2022-01-09 19:36:12 +0530

    876

---
+++ LinkedList/Middle_of_the_Linked_List.java
@@ -0,0 +1,39 @@
+//Time:O(n/2)
+//Space:O(n)
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null  ){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Time:O(n+n/2)
+    // Space:O(1)
+    // public ListNode middleNode(ListNode head) {
+    //     int size = 0;
+    //     ListNode curr = head;
+    //     while(curr != null){
+    //         size += 1;
+    //         curr = curr.next;
+    //     }
+    //     curr = head;
+    //     for(int i=0;i<size/2;i++){
+    //         curr = curr.next;
+    //     }
+    //     return curr;
+    // }
+}
+public class Middle_of_the_Linked_List {
+    
+}
