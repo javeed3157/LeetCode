839488a43bb97b7cafd039112bf4c77319ff87db
Javeed Suhail <javeed3157@gmail.com>
2022-01-10 19:40:47 +0530

    141

---
+++ LinkedList/Linked_List_Cycle.java
@@ -0,0 +1,29 @@
+// Time:O(n)
+// Space:O(1)
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+public class Linked_List_Cycle {
+    
+}
