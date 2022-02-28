f44e181a7524dbde29d7b35ad520dea44bfcbad2
Javeed Suhail <javeed3157@gmail.com>
2022-02-28 22:18:56 +0530

    605

---
+++ Arrays/Can_Place_Flowers.java
@@ -0,0 +1,26 @@
+// Time:O(n)
+// Space:O(1)
+public class Can_Place_Flowers {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    count++;
+                    if (count >= n) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return count >= n;
+    }
+    public static void main(String[] args) {
+        int[] flowerbed={1,0,0,0,1};
+        int n = 1;
+        System.out.println(canPlaceFlowers(flowerbed, n));
+    }
+}
