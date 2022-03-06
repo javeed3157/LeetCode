0626a08de6bb74e9a798ddc6c859c45162b1dec7
Javeed Suhail <javeed3157@gmail.com>
2022-03-06 21:13:31 +0530

    94

---
+++ Tree/Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,35 @@
+// Time:O(n)
+// Space:O(n)
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+        inorder(root, l);
+        return l;
+    }
+    public void inorder(TreeNode root, List<Integer> l){
+        if(root == null){
+            return;
+        }
+        inorder(root.left, l);
+        l.add(root.val);
+        inorder(root.right, l);
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Binary_Tree_Inorder_Traversal {
+    
+}
