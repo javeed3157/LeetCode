242fb4c4765d153233788d07c1fa326adb7085b8
Javeed Suhail <javeed3157@gmail.com>
2022-03-07 20:41:20 +0530

    145

---
+++ Tree/Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,35 @@
+// Time:O(n)
+// Space:O(n)
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+        postorder(root, l);
+        return l;
+    }
+    public void postorder(TreeNode root, List<Integer> l){
+        if(root == null){
+            return;
+        }
+        postorder(root.left, l);
+        postorder(root.right, l);
+        l.add(root.val);
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Binary_Tree_Postorder_Traversal {
+    
+}
