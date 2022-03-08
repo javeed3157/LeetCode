511dea659f5227fd8dba90a01252b0c04b1b1c99
Javeed Suhail <javeed3157@gmail.com>
2022-03-08 14:42:32 +0530

    145

--- Tree/Binary_Tree_Postorder_Traversal.java
+++ Tree/Binary_Tree_Postorder_Traversal.java
@@ -3,17 +3,46 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> l = new ArrayList<>();
-        postorder(root, l);
+        if(root == null){
+            return l;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(!st.isEmpty() || curr != null){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                TreeNode temp = st.peek().right;
+                if(temp == null){
+                    temp = st.pop();
+                    l.add(temp.val);
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.pop();
+                        l.add(temp.val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+            }
+        }
         return l;
     }
-    public void postorder(TreeNode root, List<Integer> l){
-        if(root == null){
-            return;
-        }
-        postorder(root.left, l);
-        postorder(root.right, l);
-        l.add(root.val);
-    }
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> l = new ArrayList<>();
+    //     postorder(root, l);
+    //     return l;
+    // }
+    // public void postorder(TreeNode root, List<Integer> l){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     postorder(root.left, l);
+    //     postorder(root.right, l);
+    //     l.add(root.val);
+    // }
 }
 /**
  * Definition for a binary tree node.
