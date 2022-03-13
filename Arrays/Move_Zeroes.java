0d5752544f57d043baaad8d932396aa436d9bd0c
Javeed Suhail <javeed3157@gmail.com>
2022-03-13 10:27:35 +0530

    283

--- Arrays/Move_Zeroes.java
+++ Arrays/Move_Zeroes.java
@@ -1,33 +1,23 @@
 // Time:O(n)
-// Space:O(n)
+// Space:O(1)
 public class Move_Zeroes{
     public static void moveZeroes(int[] nums) {
-        int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                count += 1;
-            }
-        }
-        int[] new_arr = new int[nums.length];
-        int j = 0;
+        int pos = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != 0){
-                new_arr[j] = nums[i];
-                j += 1;
+                int temp = nums[pos];
+                nums[pos] = nums[i];
+                nums[i] = temp;
+                pos += 1;
             }
         }
-        while(j < nums.length){
-            new_arr[j] = 0;
-            j += 1;
-        }
         for(int i = 0; i < nums.length; i++){
-            nums[i] = new_arr[i];
             System.out.print(nums[i] +" ");
         }
         
     }
     public static void main(String[] args) {
-        int[] nums = {0,1,0,3,1,2};
+        int[] nums = {0,1,0,3,12};
         moveZeroes(nums);
     }
 }
