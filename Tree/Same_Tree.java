069aefb053b473564fbea8c04e7592e2986b907d
Javeed Suhail <javeed3157@gmail.com>
2022-03-21 21:56:40 +0530

    100

---
+++ Tree/Same_Tree.java
@@ -0,0 +1,31 @@
+// Time:O(n)
+// Space:O(n)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        return p != null && q != null && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+public class Same_Tree{
+
+    public static void main(String[] args) {
+        
+    }
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+}
