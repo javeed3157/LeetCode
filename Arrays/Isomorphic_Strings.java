780d55408b9ff32bf3b7b8bb8ea182e21064c1c9
Javeed Suhail <javeed3157@gmail.com>
2022-08-16 21:16:20 +0530

    205

--- Arrays/Isomorphic_Strings.java
+++ Arrays/Isomorphic_Strings.java
@@ -1,9 +1,10 @@
-// Time:O(n^2)
+// Time:O(n)
 // Space:O(n)
 import java.util.*;
 public class Isomorphic_Strings {
     public static boolean isIsomorphic(String s, String t) {
         HashMap<Character, Character> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
         for(int i = 0; i < s.length(); i++){
             if(map.containsKey(s.charAt(i))){
                 if(map.get(s.charAt(i)) != t.charAt(i)){
@@ -11,14 +12,34 @@ public class Isomorphic_Strings {
                 }
             }
             else{
-                if(map.containsValue(t.charAt(i))){
+                if(set.contains(t.charAt(i))){
                     return false;
                 }
                 map.put(s.charAt(i), t.charAt(i));
+                set.add(t.charAt(i));
             }
         }
         return true;
     }
+    // Time:O(n^2)
+    // Space:O(n)
+    // public static boolean isIsomorphic(String s, String t) {
+    //     HashMap<Character, Character> map = new HashMap<>();
+    //     for(int i = 0; i < s.length(); i++){
+    //         if(map.containsKey(s.charAt(i))){
+    //             if(map.get(s.charAt(i)) != t.charAt(i)){
+    //                 return false;
+    //             }
+    //         }
+    //         else{
+    //             if(map.containsValue(t.charAt(i))){
+    //                 return false;
+    //             }
+    //             map.put(s.charAt(i), t.charAt(i));
+    //         }
+    //     }
+    //     return true;
+    // }
     public static void main(String[] args) {
         String s1 = "egg";
         String s2 = "add";
