ca8668f722a91407e83db999f8a6a381f1e64b37
Javeed Suhail <javeed3157@gmail.com>
2022-08-23 09:43:34 +0530

    104

--- Tree/Maximum_Depth_of_Binary_Tree.java
+++ Tree/Maximum_Depth_of_Binary_Tree.java
@@ -1,24 +1,32 @@
 // Time:O(n)
-// Space:O(n)
+// Space:O(h)
 class Solution {
     public int maxDepth(TreeNode root) {
-        return height(root) + 1;
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
-    public int height(TreeNode node){
-        if(node == null){
-            return -1;
-        }
-        int lh = height(node.left);
-        int rh = height(node.right);
-        int h = 0;
-        if(lh > rh){
-            h = 1 + lh;
-        }
-        else{
-            h = 1 + rh;
-        }
-        return h;
-    }
+    // Time:O(n)
+    // Space:O(n)
+    // public int maxDepth(TreeNode root) {
+    //     return height(root) + 1;
+    // }
+    // public int height(TreeNode node){
+    //     if(node == null){
+    //         return -1;
+    //     }
+    //     int lh = height(node.left);
+    //     int rh = height(node.right);
+    //     int h = 0;
+    //     if(lh > rh){
+    //         h = 1 + lh;
+    //     }
+    //     else{
+    //         h = 1 + rh;
+    //     }
+    //     return h;
+    // }
 }
 public class Maximum_Depth_of_Binary_Tree {
     
