db0f6ca735e3fb54eb28609765e4d5b7243be5db
Javeed Suhail <javeed3157@gmail.com>
2022-08-24 09:46:43 +0530

    543

--- Tree/Balanced_Binary_Tree.java
+++ Tree/Balanced_Binary_Tree.java
@@ -1,5 +1,21 @@
 // Time:O(n)
 // Space:O(h)
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] maxi = {0};
+        height(root, maxi);
+        return maxi[0];
+    }
+    public int height(TreeNode node, int[] maxi){
+        if(node == null){
+            return 0;
+        }
+        int lh = height(node.left, maxi);
+        int rh = height(node.right, maxi);
+        maxi[0] = Math.max(maxi[0], lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
+}
 /**
 * Definition for a binary tree node.
 * public class TreeNode {
@@ -16,25 +32,25 @@
 * }
 */
 //
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return height(root) != -1;
-    }
-    public int height(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        int left = height(root.left);
-        int right = height(root.right);
-        if(left == -1 || right == -1){
-            return -1;
-        }
-        if(Math.abs(left - right) > 1){
-            return -1;
-        }
-        return 1 + Math.max(left, right);
-    }
-}
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         return height(root) != -1;
+//     }
+//     public int height(TreeNode root){
+//         if(root == null){
+//             return 0;
+//         }
+//         int left = height(root.left);
+//         int right = height(root.right);
+//         if(left == -1 || right == -1){
+//             return -1;
+//         }
+//         if(Math.abs(left - right) > 1){
+//             return -1;
+//         }
+//         return 1 + Math.max(left, right);
+//     }
+// }
 public class Balanced_Binary_Tree {
     
 }
