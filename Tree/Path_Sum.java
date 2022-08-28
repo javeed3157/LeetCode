013fe5ef6541242d7f614e346ca4debca9da2a8f
Javeed Suhail <javeed3157@gmail.com>
2022-08-28 22:54:03 +0530

    112

---
+++ Tree/Path_Sum.java
@@ -0,0 +1,31 @@
+// Time:O(n)
+// Space:O(n)
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        if(root.left == null && root.right == null){
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left , targetSum - root.val) || hasPathSum(root.right , targetSum - root.val);
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Path_Sum {
+    
+}
