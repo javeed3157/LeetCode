fde6f5236988ca8ce8d9c44a8b1c827489e675f8
Javeed Suhail <javeed3157@gmail.com>
2022-10-17 08:04:40 +0530

    1832

--- String/Check_if_the_Sentence_Is_Pangram.java
+++ String/Check_if_the_Sentence_Is_Pangram.java
@@ -1,21 +1,29 @@
 //Time:O(n)
 //Space:O(26)
+import java.util.*;
 public class Check_if_the_Sentence_Is_Pangram {
     public static boolean checkIfPangram(String sentence) {
-        char arr[]=new char[26];
-        for(int i=0;i<sentence.length();i++){
-            arr[(int)sentence.charAt(i)-97]=1;
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < sentence.length(); i++){
+            set.add(sentence.charAt(i));
         }
-        for(int i=0;i<26;i++){
-            if(arr[i]>0){
-                continue;
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
+        return set.size() == 26;
     }
+    // public static boolean checkIfPangram(String sentence) {
+        // char arr[]=new char[26];
+        // for(int i=0;i<sentence.length();i++){
+            // arr[(int)sentence.charAt(i)-97]=1;
+        // }
+        // for(int i=0;i<26;i++){
+            // if(arr[i]>0){
+                // continue;
+            // }
+            // else{
+                // return false;
+            // }
+        // }
+        // return true;
+    // }
     public static void main(String[] args) {
         String s="thequickbrownfoxjumpsoverthelazydog";
         System.out.println(checkIfPangram(s));
