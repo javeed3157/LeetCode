f83c1bf11027982f0552baac243a3187aad5efda
Javeed Suhail <javeed3157@gmail.com>
2022-12-01 14:21:40 +0530

    1704'

---
+++ String/Determine_if_String_Halves_Are_Alike.java
@@ -0,0 +1,26 @@
+// Time:O(n)
+// Space:O(1)
+public class Determine_if_String_Halves_Are_Alike {
+    public static boolean halvesAreAlike(String s) {
+        int c1 = 0;
+        int c2 = 0;
+        int n = s.length();
+        for(int i = 0 ; i < n; i++){
+            if(i < n/2){
+                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
+                    c1 += 1;
+                }
+            }
+            else{
+                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
+                    c2 += 1;
+                }
+            }
+        }
+        return c1 == c2;
+    }
+    public static void main(String[] args) {
+        String s = "book";
+        System.out.println(halvesAreAlike(s));
+    }
+}
